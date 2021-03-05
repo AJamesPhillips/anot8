15540d5a5bf13cc135143fa6845ba8ff0db4f91b
Alexander James Phillips <ajp.global@gmail.com>
2021-03-05 09:09:48 +0000

    Add stub code for handling user specific annotation files

---
+++ src/server/annotations_by_specific_users.py
@@ -0,0 +1,10 @@
+
+
+
+def upsert_user_specific_meta_data_annotations_file (vault_config, annotations_relative_file_path, user_name):
+    pass
+
+
+
+def ensure_user_in_list_of_annotation_user_names (meta_data, user_name):
+    return meta_data
