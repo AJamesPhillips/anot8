eb0460af3154062c39789ff4f15f15fd6f9c573a
Alexander James Phillips <ajp.global@gmail.com>
2022-02-15 10:48:07 +0000

    Do not show labels list when no labels

--- src/client/src/components/side_panel/labels_list/Labels.tsx
+++ src/client/src/components/side_panel/labels_list/Labels.tsx
@@ -9,8 +9,11 @@ import { LabelsList } from "./LabelsList"
 
 
 const map_state = (state: State) => ({
+    have_any_labels: Object.keys(state.labels.labels_by_id).length > 0,
     highlighting_used_labels: state.labels.highlighting_used_labels,
     search_string: state.labels.search_string,
+    running_locally: state.running_locally,
+    is_from_url: !!state.routing.url,
 })
 type Props = ReturnType<typeof map_state>
 const connector = connect(map_state)
@@ -20,6 +23,20 @@ const store = get_store()
 
 function _Labels (props: Props)
 {
+    if (!props.have_any_labels)
+    {
+        const title = props.is_from_url
+            ? "A vault of PDFs also specifies the labels so when a PDF is loaded by a URL there is no vault and no labels"
+            : (props.running_locally
+                ? "Add labels to your vault config json file"
+                : "Editing a vault is not yet supported on anot8.org, must manually edit vault locally."
+            )
+
+        return <div style={{ color: "grey", fontSize: "small", cursor: "default" }} title={title}>
+            No vault labels
+        </div>
+    }
+
     return <div className={props.highlighting_used_labels ? "highlight_used_labels" : ""}>
         <h3>List of labels:</h3>
         <input
